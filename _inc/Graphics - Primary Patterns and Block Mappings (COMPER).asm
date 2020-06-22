160c4b8495c428ffa4da3513f50b8285422dcef8
Cyber Axe <cyberaxe@cyberaxe.net>
2020-06-23 00:54:14 +0100

    Added Code for COMPER Compression, Uncompressed, and seperated level art

---
+++ _inc/Graphics - Primary Patterns and Block Mappings (COMPER).asm
@@ -0,0 +1,75 @@
+; ---------------------------------------------------------------------------
+; Compressed graphics - primary patterns and block mappings
+; ---------------------------------------------------------------------------
+Blk16_GHZ:		incbin	"map16\GHZ.bin"
+		even
+Gra_Title:		incbin	"artcom\8x8 - GHZ1.bin"	; GHZ primary patterns
+		even
+Gra_GHZ:	  	incbin	"artcom\8x8 - GHZ.bin"	; GHZ combined patterns
+		even
+	if TweakUncompressedChunkMapping=0
+Blk256_GHZ:		incbin	"map256\GHZ.bin"
+	else
+Blk256_GHZ:		incbin	"map256_u\Uncompressed\GHZ.bin"
+	endc ; if TweakUncompressedChunkMapping=0
+		even
+Blk16_LZ:			incbin	"map16\LZ.bin"
+		even
+Gra_LZ:				incbin	"artcom\8x8 - LZ.bin"	; LZ primary patterns
+		even
+	if TweakUncompressedChunkMapping=0
+Blk256_LZ:		incbin	"map256\LZ.bin"
+	else
+Blk256_LZ:		incbin	"map256_u\Uncompressed\LZ.bin"
+	endc ; if TweakUncompressedChunkMapping=0
+		even
+Blk16_MZ:			incbin	"map16\MZ.bin"
+		even
+Gra_MZ:				incbin	"artcom\8x8 - MZ.bin"	; MZ primary patterns
+		even
+Blk256_MZ:
+	if TweakUncompressedChunkMapping=0 && Revision=0
+							incbin	"map256\MZ.bin"
+	elseif TweakUncompressedChunkMapping=0 && Revision>0
+							incbin	"map256\MZ (JP1).bin"
+	elseif TweakUncompressedChunkMapping>0 && Revision=0
+							incbin	"map256_u\Uncompressed\MZ.bin"
+	else
+							incbin	"map256_u\Uncompressed\MZ (JP1).bin"
+	endc ; if TweakUncompressedChunkMapping=0 && Revision=0
+		even
+Blk16_SLZ:		incbin	"map16\SLZ.bin"
+		even
+Gra_SLZ:			incbin	"artcom\8x8 - SLZ.bin"	; SLZ primary patterns
+		even
+	if TweakUncompressedChunkMapping=0
+Blk256_SLZ:		incbin	"map256\SLZ.bin"
+	else
+Blk256_SLZ:		incbin	"map256_u\Uncompressed\SLZ.bin"
+	endc ; if TweakUncompressedChunkMapping=0
+		even
+Blk16_SYZ:		incbin	"map16\SYZ.bin"
+		even
+Gra_SYZ:			incbin	"artcom\8x8 - SYZ.bin"	; SYZ primary patterns
+		even
+	if TweakUncompressedChunkMapping=0
+Blk256_SYZ:		incbin	"map256\SYZ.bin"
+	else
+Blk256_SYZ:		incbin	"map256_u\Uncompressed\SYZ.bin"
+	endc ; if TweakUncompressedChunkMapping=0
+		even
+Blk16_SBZ:		incbin	"map16\SBZ.bin"
+		even
+Gra_SBZ:			incbin	"artcom\8x8 - SBZ.bin"	; SBZ primary patterns
+		even
+Blk256_SBZ:
+	if TweakUncompressedChunkMapping=0 && Revision=0
+							incbin	"map256\SBZ.bin"
+	elseif TweakUncompressedChunkMapping=0 && Revision>0
+							incbin	"map256\SBZ (JP1).bin"
+	elseif TweakUncompressedChunkMapping>0 && Revision=0
+							incbin	"map256_u\Uncompressed\SBZ.bin"
+	else
+							incbin	"map256_u\Uncompressed\SBZ (JP1).bin"
+	endc ; if TweakUncompressedChunkMapping=0 && Revision=0
+		even
