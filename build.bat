2fbabcdcde488ffa1864432bb4e4ba6b6a4e6525
Cyber Axe <cyberaxe@cyberaxe.net>
2020-06-23 11:23:20 +0100

    Updated to output Errors to console and add output folder for rom

--- build.bat
+++ build.bat
@@ -1,5 +1,16 @@
-@echo off
+@SETLOCAL EnableExtensions
+@ECHO OFF
+@CD /d %~dp0
 
-IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL
+@SET OUTPUT_PATH=%~1
+
+@IF EXIST "errors.txt" DEL /q "errors.txt" 2>&1
+@IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL 2>&1
+
+REM Compile ROM
 asm68k /k /p /o ae- sonic.asm, s1built.bin >errors.txt, , sonic.lst
+@FINDSTR /m "0 error(s)" errors.txt >NUL 2>&1 && IF NOT "%OUTPUT_PATH%"=="" IF EXIST "%OUTPUT_PATH%\" COPY /y s1built.bin "%OUTPUT_PATH%\Sonic 1 (Github).bin" >NUL 2>&1
+@TYPE errors.txt
+
+REM Fix ROM Header
 fixheadr.exe s1built.bin
