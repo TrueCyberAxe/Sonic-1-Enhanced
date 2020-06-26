d8fd4ae3d08f427d42001f2b76a5324ad5135f58
Cyber Axe <cyberaxe@cyberaxe.net>
2020-06-26 19:00:44 +0100

    Helpful Tweak for Testing

--- build.bat
+++ build.bat
@@ -4,14 +4,14 @@
 
 @SET OUTPUT_PATH=%~1
 
-@REM Echo a Seperator for easier console browsing
-@ECHO ==========================================================================
-
 @IF EXIST "errors.txt" DEL /q "errors.txt" 2>&1
 @IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL 2>&1
 
 REM Compile ROM
+@REM Echo a Seperator for easier console browsing
+@ECHO ================================================================================
 @ECHO Reference - Checksum: Rev0 = 264A, Rev1 = AFC7, Rev2 = 2F9C - Size 6FEFF / 7FFFF
+@ECHO ================================================================================
 asm68k /k /p /o ae- sonic.asm, s1built.bin >errors.txt, , sonic.lst
 @TYPE errors.txt
 
