6a3e58f97a394f5e2cc3d5849bad5d66291951e8
Cyber Axe <cyberaxe@cyberaxe.net>
2020-06-27 00:42:08 +0100

    Cleanup

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 @ECHO OFF
 @CD /d %~DP0
 
-@SET OUTPUT_PATH=%~1
+@SET "OUTPUT_PATH=%~1"
 
 @IF EXIST "errors.txt" DEL /q "errors.txt" 2>&1
 @IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL 2>&1
@@ -23,7 +23,7 @@ IF NOT "%ERROR%"=="FALSE" GOTO :END
 @ECHO.
 @ECHO ==================================================================================
 REM Fix ROM Header
-fixheadr.exe s1built.bin | FIND /I "Checksum Applied:    0"
+fixheadr.exe s1built.bin
 
 REM Copy ROM to output Folder
 IF NOT "%OUTPUT_PATH%"=="" IF EXIST "%OUTPUT_PATH%\" COPY /y s1built.bin "%OUTPUT_PATH%\Sonic 1 (Github).bin" >NUL 2>&1
