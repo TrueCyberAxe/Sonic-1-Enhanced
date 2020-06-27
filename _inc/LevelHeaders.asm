19fa9abb2ae752b095c2436614cd75e3ffc5a358
Cyber Axe <cyberaxe@cyberaxe.net>
2020-06-27 01:09:52 +0100

    Update

--- _inc/LevelHeaders.asm
+++ _inc/LevelHeaders.asm
@@ -17,7 +17,7 @@ lhead:	macro plc1,lvlgfx,plc2,sixteen,twofivesix,music,pal
 ;		1st PLC				2nd PLC				256x256 data			palette
 ;				level gfx*			16x16 data			music*
 
-	lhead	plcid_GHZ,	Gra_GHZ,			plcid_GHZ2,	Blk16_GHZ,	Blk256_GHZ,	bgm_GHZ,	palid_GHZ		; Green Hill
+	lhead	plcid_GHZ,	Gra_GHZ,			plcid_GHZ,	Blk16_GHZ,	Blk256_GHZ,	bgm_GHZ,	palid_GHZ		; Green Hill
 	lhead	plcid_LZ,		Gra_LZ,				plcid_LZ2,	Blk16_LZ,		Blk256_LZ,	bgm_LZ,		palid_LZ		; Labyrinth
 	lhead	plcid_MZ,		Gra_MZ,				plcid_MZ2,	Blk16_MZ,		Blk256_MZ,	bgm_MZ,		palid_MZ		; Marble
 	lhead	plcid_SLZ,	Gra_SLZ,			plcid_SLZ2,	Blk16_SLZ,	Blk256_SLZ,	bgm_SLZ,	palid_SLZ		; Star Light
