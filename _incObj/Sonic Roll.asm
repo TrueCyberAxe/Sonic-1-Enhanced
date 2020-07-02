f6c39ba9208edec172a99067ff799a5067db552f
Cyber Axe <cyberaxe@cyberaxe.net>
2020-07-02 23:30:53 +0100

    Slow Ducking from S3

--- _incObj/Sonic Roll.asm
+++ _incObj/Sonic Roll.asm
@@ -13,22 +13,38 @@ Sonic_Roll:
 		neg.w	d0
 
 	@ispositive:
-		cmpi.w	#$80,d0		; is Sonic moving at $80 speed or faster?
-		bcs.s	@noroll		; if not, branch
+	if TweakSlowDucking=0
+		cmpi.w	#$80,d0															; is Sonic moving at $80 speed or faster?
+		bcs.s	@noroll																; if not, branch
 		move.b	(v_jpadhold2).w,d0
-		andi.b	#btnL+btnR,d0	; is left/right	being pressed?
-		bne.s	@noroll		; if yes, branch
-		btst	#bitDn,(v_jpadhold2).w ; is down being pressed?
-		bne.s	Sonic_ChkRoll	; if yes, branch
+		andi.b	#btnL+btnR,d0												; is left/right	being pressed?
+		bne.s	@noroll																; if yes, branch
+		btst	#bitDn,(v_jpadhold2).w 								; is down being pressed?
+		bne.s	Sonic_ChkRoll													; if yes, branch
+	else
+		btst	#bitDn,(v_jpadhold2).w 								; is down being pressed?
+		beq.s	@noroll																; if yes, branch
+		move.b	(v_jpadhold2).w,d0
+		andi.b	#btnL+btnR,d0												; is left/right	being pressed?
+		bne.s	@noroll																; if yes, branch
+		move.w $14(a0),d0
+		bpl.s @cont 																; If ground speed is positive, continue
+		neg.w d0 																		; If not, negate it to get the absolute value
+
+	@cont:
+		cmpi.w    #$100,d0    											; is Sonic moving at $100 speed or faster?
+		bhi.s    Sonic_ChkRoll    									; if yes, branch
+		move.b    #id_Duck,obAnim(a0)    						; use "ducking" animation
+	endc
 
 	@noroll:
-		rts	
+		rts
 ; ===========================================================================
 
 Sonic_ChkRoll:
 		btst	#2,obStatus(a0)	; is Sonic already rolling?
 		beq.s	@roll		; if not, branch
-		rts	
+		rts
 ; ===========================================================================
 
 @roll:
@@ -43,5 +59,5 @@ Sonic_ChkRoll:
 		move.w	#$200,obInertia(a0) ; set inertia if 0
 
 	@ismoving:
-		rts	
+		rts
 ; End of function Sonic_Roll
