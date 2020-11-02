4effbf151626dcca90e234649f7fed6359e5f5f8
Cyber Axe <cyberaxe@cyberaxe.net>
2020-11-02 15:58:56 +0000

    Fix compare.sh for debugging and correct batch files extensions

--- compare.sh
+++ compare.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-# (
+if [ -z "$1" ]; then
   export FILE1='revision2.lst'
-  export FILE2='sonic.lst'
-  export FILTER='awk -F";" "{print substr(\$1,1,35)gensub(\"[[:space:]]+\", \" \", \"g\",substr(\$1,36))}"'
-  export FILTER="cut -c 1-36"
-  export FILTER="grep -Ev '^00000000 ' | awk -F';' '{print substr(\$1,1,35)gensub(\"[[:space:]]+\", \" \", \"g\",substr(\$1,36))}'"
+else
+  export FILE1="$1"
+fi
+export FILE2='sonic.lst'
+#export FILTER='awk -F";" "{print substr(\$1,1,35)gensub(\"[[:space:]]+\", \" \", \"g\",substr(\$1,36))}"'
+#export FILTER="cut -c 1-36"
+export FILTER="grep -Ev '^00000000 ' | awk -F';' '{print substr(\$1,1,35)gensub(\"[[:space:]]+\", \" \", \"g\",substr(\$1,36))}' | grep -E '^[0-9A-F]{8} [0-9A-F]' | uniq -w36 -u"
 
-  # eval "grep -E '^\S{8} \S' $FILE1 | $FILTER"
-  # diff -EwburqN
-  eval "diff -ibuwIBEZ <(grep -E '^\S{8} \S' $FILE1 | $FILTER)  <(grep -E '^\S{8} \S' $FILE2 | $FILTER)" | grep -E "^[-+][0-9A-F]{8}"| cut -c 2-37 | sort | uniq -w36 -u > diff.lst
-  # awk '{print substr($1,1,35)gensub("[[:space:]]+", " ", "g",substr($1,36))}' > diff.lst
-# )
-
-# eval "diff -ibwIBEZ <(grep -E '^\S{8} \S' $FILE1 | $FILTER)  <(grep -E '^\S{8} \S' $FILE2 | $FILTER)" | (
-#     lnum=0;
-#     while read lprint; do
-#         while [ $lnum -lt $lprint ]; do read line <&3; ((lnum++)); done;
-#         echo $line;
-#     done
-# ) 3<md5sums.sort.XXX
+# Echo out all changed instructions to diff.lst
+eval "diff -ibuwIBEZ <(grep -E '^\S{8} \S' $FILE1 | $FILTER)  <(grep -E '^\S{8} \S' $FILE2 | $FILTER)" | grep -E '^[ +-]' | sort | uniq -s1 -w36 -c | grep -E '^\s+[1]\s+[+-]' | cut -c10- | sort > diff.lst
